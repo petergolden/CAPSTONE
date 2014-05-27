6f155cbe3b2971e3f5df42720c631ff983e30284
kturnbull <kforesman@gmail.com>
2014-05-26 21:40:47 -0500

    Full model for bagging

--- BaggingCode.R
+++ BaggingCode.R
@@ -5,14 +5,16 @@ library(ipred)
 
 load("imputedOrdersPostTransformation.rdata")
 
-#eliminate cases where timeToDeliver is NA
+#eliminate cases where timeToDeliver/customerAge is NA
 orders.train <- orders.train[complete.cases(orders.train[,17]),]
-# creating an even smaller dataset by only having variables used by the model
-# (based on LR BE)
+orders.train <- orders.train[complete.cases(orders.train[,'customerAge']),]
+# creating an even smaller dataset by only having variables used
+# this drops actual dates and a lot of the size stuff
 str(orders.train)
-orders <- cbind(orders.train[8:10],orders.train[14:18],orders.train[25],orders.train[27:28],
-                orders.train[35],orders.train[37],orders.train[39],orders.train[42:43],
-                orders.train[45:47],orders.train[54:55])
+orders <- cbind(orders.train[1],orders.train[3:5],orders.train[7:10],
+                orders.train[12],orders.train[14:19],
+                orders.train[25:28],orders.train[35:47],
+                orders.train[54:55])
 str(orders)
 
 #train/test split, 70/30
@@ -26,7 +28,79 @@ gc()
 
 save.image(file="KT_initial_workspace.RData")
 
-################ Bagging Model ###################
+summary(train)
+
+################ Bagging Model - Full Model ###################
+# Reference:
+# http://heuristically.wordpress.com/2009/12/23/compare-performance-machine-learning-classifiers-r/
+
+# fit a bagging model to training set 
+bagging.model <- bagging(returnShipment ~ ., data = train)
+summary(bagging.model)
+
+predict.bag.train <- predict(bagging.model, type="prob")
+train.rocbag.prediction <- prediction(predict.bag.train, train$returnShipment)
+predict.bag.test <- predict(bagging.model, type="prob", newdata=test)
+test.rocbag.prediction <- prediction(predict.bag.test, test$returnShipment)
+
+# true positive rate and false positive rate for ROC curve plotting
+train.rocbag <- performance(train.rocbag.prediction, "tpr","fpr")
+test.rocbag <- performance(test.rocbag.prediction, "tpr","fpr")
+
+# do the ROC plot
+pdf("Bagging_ROC_Full.pdf")
+plot(train.rocbag, col="blue", main = "ROC Bagging")
+plot(test.rocbag, col="red", add = TRUE)
+abline(c(0,1))
+legend("bottomright",c(paste("Training: AUC =",round(as.numeric(
+  performance(train.rocbag.prediction,"auc")@y.values),4)),
+  paste("Test: AUC =",round(as.numeric(
+    performance(test.rocbag.prediction,"auc")@y.values),4))),
+  fill=(c("blue","red")))
+dev.off()
+
+# And then a lift chart
+pdf("Bagging_Lift_Full.pdf")
+train.liftbag <- performance(train.rocbag.prediction, "lift","rpp")
+test.liftbag <- performance(test.rocbag.prediction, "lift","rpp")
+plot(train.liftbag, col="blue", main = "Lift Curve Bagging")
+plot(test.liftbag, col="red", add = TRUE)
+legend("bottomleft",c("Training","Test"),fill=(c("blue","red")))
+dev.off()
+
+rmse <- function(observed,predicted) {
+  sqrt(mean((observed-predicted)^2))
+}
+
+rmse(test$returnShipment,predict.bag.test) # 0.4079
+
+# clean up everything except stuff for combined ROC/rmse:
+# test.rocbag
+# test.rocbag.prediction
+# predict.bag.test
+# test
+remove(train,bagging.model,predict.bag.train,test.liftbag,
+       train.rocbag,train.rocbag.prediction,train.liftbag)
+
+save.image("bagging_full.RData")
+
+#------------------#
+# Confusion Matrix #  
+#------------------#
+
+load("bagging.RData")
+# Using a p=.5 cutoff initially
+predictions<-cut(predict.bag.test, c(-Inf,0.5,Inf), labels=c("Keep","Return"))
+bagActuals <- factor(test$returnShipment,
+                     levels = c(0,1),
+                     labels = c("Keep", "Return"))
+confusionMatrix(predictions, bagActuals)
+
+# Get r squared
+1 - sum((test$returnShipment-predict.bag.test)^2)/
+  sum((test$returnShipment-mean(test$returnShipment))^2)
+
+################ Bagging Model - LR BE Variables ###################
 # Reference:
 # http://heuristically.wordpress.com/2009/12/23/compare-performance-machine-learning-classifiers-r/
 
@@ -58,7 +132,7 @@ abline(c(0,1))
 legend("bottomright",c(paste("Training: AUC =",round(as.numeric(
   performance(train.rocbag.prediction,"auc")@y.values),4)),
   paste("Test: AUC =",round(as.numeric(
-  performance(test.rocbag.prediction,"auc")@y.values),4))),
+    performance(test.rocbag.prediction,"auc")@y.values),4))),
   fill=(c("blue","red")))
 dev.off()
 
@@ -95,8 +169,8 @@ load("bagging.RData")
 # Using a p=.5 cutoff initially
 predictions<-cut(predict.bag.test, c(-Inf,0.6,Inf), labels=c("Keep","Return"))
 bagActuals <- factor(test$returnShipment,
-                    levels = c(0,1),
-                    labels = c("Keep", "Return"))
+                     levels = c(0,1),
+                     labels = c("Keep", "Return"))
 confusionMatrix(predictions, bagActuals)
 
 # Get r squared
